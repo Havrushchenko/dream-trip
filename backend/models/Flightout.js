98c8b031e37d3693b66eda3b061cab8aaf32a24b
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2023-01-13 12:06:59 +0200

    setup the database

---
+++ backend/models/Flightout.js
@@ -0,0 +1,60 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../../config/connection');
+
+// create our User model
+class Flightout extends Model { }
+
+// define table columns and configuration
+Flightout.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        flight_number: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true
+        },
+        departure: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        destination: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        flight_number: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        airplane_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        departure_time: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        arrival_time: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+    },
+    {
+        // pass in our imported sequelize connection (the direct connection to our database)
+        sequelize,
+        // don't automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        // don't pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+        underscored: true,
+        // make it so our model name stays lowercase in the database
+        modelName: 'flightout'
+    }
+);
+
+module.exports = Flightout;
