4c9121498e4f7a13191e24f4d3c998d897e6c622
miklywek <miklywek@gmail.com>
2023-01-13 16:35:00 +0200

    add login-form

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,27 +1,38 @@
 async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector("#username-signup").value.trim();
-    const email = document.querySelector("#email-signup").value.trim();
-    const password = document.querySelector("#password-signup").value.trim();
-  
-    if (username && email && password) {
-      console.log(username);
-      // fetch("/api/login", {
-      //   method: "post",
-      //   body: JSON.stringify({
-      //     username,
-      //     email,
-      //     password,
-      //   }),
-      //   headers: { "Content-Type": "application/json" },
-      // }).then((response) => {
-      //   console.log(response);
-      // });
-    }
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && email && password) {
+    console.log(username);
+    // fetch("/api/login", {
+    //   method: "post",
+    //   body: JSON.stringify({
+    //     username,
+    //     email,
+    //     password,
+    //   }),
+    //   headers: { "Content-Type": "application/json" },
+    // }).then((response) => {
+    //   console.log(response);
+    // });
   }
-  
-  document
-    .querySelector(".signup-form")
-    .addEventListener("submit", signupFormHandler);
-  
+}
+async function loginFormHandler(event) {
+  event.preventDefault();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    console.log(email);
+  }
+}
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
