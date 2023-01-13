6ab31445692e8107508012474630065eaceffeda
miklywek <miklywek@gmail.com>
2023-01-13 14:52:41 +0200

    creat signup router

---
+++ backend/controllers/api/user-routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const { User } = require("../../models/User");
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
-const express = require('express');
-const sequelize = require('./config/connection');
+const express = require("express");
+const sequelize = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+const session = require("express-session");
+const Sequelize = require("connect-session-sequelize")(session.Store);
 
+const sess = {
+  secret: "Super secret ",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  satore: new Sequelize({
+    db: sequelize,
+  }),
+};
 const app = express();
+app.use(session(sess));
+app.use(require('./controllers'));
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
